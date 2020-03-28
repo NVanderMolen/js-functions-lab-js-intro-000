8a31c3b0a8eb7c819f02c09a381cfd21ae482a3d
NICHOLAS R VANDERMOLEN <tree.vandermolen@gmail.com>
2020-03-28 21:47:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+
+function happyHolidays(){
+  return ("Happy Holidays!");
+}
+function happyHolidaysTo(name){
+  return (`Happy Holidays, ${name}!`);
+}
+function happyCustomHolidayTo (holiday, name){
+  return (`Happy ${holiday}, ${name}!`);
+}
+function holidayCountdown (holiday, days){
+  return (`It's ${days} days until ${holiday}!`);
+}
